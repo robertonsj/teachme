1ed115977d1b69e57471bf05af7fe629250c94fd
Roberto Junior <robertonsj31@gmail.com>
2023-04-19 09:43:16 -0300

    PageHeader component created

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,11 @@
+import PageHeader from "../../components/PageHeader";
+import "./styles.css";
+
 function TeacherForm() {
-  return <div>TeacherForm</div>;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader />;
+    </div>
+  );
 }
 export default TeacherForm;
