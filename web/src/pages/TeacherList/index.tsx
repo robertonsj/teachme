375c985f43cd58e6478b69d2e477298d163d6629
Roberto Junior <robertonsj31@gmail.com>
2023-04-26 15:37:34 -0300

    Working on Input component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,28 +1,18 @@
+import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 
 import "./styles.css";
 // import "../../assets/styles/global.css";
 
-import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
-
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os Teachers disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Time</label>
-            <input type="text" id="time" />
-          </div>
+          <Input name="subject" label="Matéria" />
+          <Input name="week_day" label="Dia da semana" />
+          <Input name="time" label="Hora" />
         </form>
       </PageHeader>
 
