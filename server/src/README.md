af3641bb1426f4089f9e5b8ce8410c853781be39
Roberto Junior <robertonsj31@gmail.com>
2023-05-04 15:34:31 -0300

    Starting Server Migrations

---
+++ server/src/README.md
@@ -0,0 +1,9 @@
+# Funcionalities
+
+## Aulas
+
+- Route to create class.
+- Route to list classes.
+  i. Filter on subject,
+  ii. Filter on Day of week
+  iii. Filter for time
---
+++ server/src/database/migrations/00_create_users.ts
@@ -0,0 +1,34 @@
+import Knex from "knex";
+// import Knex, { knex } from "knex";
+
+exports.up = function (knex) {
+  return knex.schema.createTable("classes", function (table) {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("avatar").notNullable();
+    table.string("whatsapp").notNullable();
+    table.string("bio").notNullable();
+  });
+};
+
+// import Knex, { knex } from "knex";
+
+// export async function up(knex: knex) {
+//   return knex.schema.createTable("classes", (table) => {
+//     table.increments("id").primary();
+//     table.string("subject").notNullable();
+//     table.decimal("cost").notNullable();
+
+//     table
+//       .integer("user_id")
+//       .notNullable()
+//       .references("id")
+//       .inTable("users")
+//       .onUpdate("CASCADE")
+//       .onDelete("CASCADE");
+//   });
+// }
+
+// export async function down(knex: Knex) {
+//   return knex.schema.dropTable("classes");
+// }
